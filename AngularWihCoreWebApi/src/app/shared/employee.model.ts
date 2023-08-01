93c4cc8568f4acc41f3cd5fb708706f351c62177
saroshfaisalkhan <saroshfaisalkhan@gmail.com>
2023-07-31 18:43:58 -0700

    Employee Model, Employee Service Created and HttpClientModule Inserted INTO app module.ts

---
+++ AngularWihCoreWebApi/src/app/shared/employee.model.ts
@@ -0,0 +1,19 @@
+export class Employee {
+    id:number=0;
+    name:string='';
+    lastname:string='';
+    email:string='';
+    age:number=0;
+    doj:any;
+    gender:string='';
+    ismarried:boolean=false;
+    isactive:boolean=false;
+    designationId:number=0;
+    designation:string='';
+}
+
+export class Designation{
+    id:number=0;
+    designation:string='';
+}
+
